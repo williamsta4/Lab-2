055b17e78e12cd28522ea1b1135561f31e1dc303
williamsta4 <williamsta4@etsu.edu>
2022-09-01 17:33:37 -0400

    Question 3 answer

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,13 +1,17 @@
-﻿Console.WriteLine("Please enter your first number: ");
-string input1Str = Console.ReadLine();
+﻿Console.WriteLine("-------------------------------------------------------------");
+Console.WriteLine("Type: \t Byte(s) of memory: \t\t Min: \t\t Max");
+Console.WriteLine("-------------------------------------------------------------");
+Console.WriteLine($"sbyte: \t {sizeof(sbyte)}\t\t\t\t{sbyte.MinValue} \t\t {sbyte.MaxValue}" );
+Console.WriteLine($"byte:  \t {sizeof(byte)}\t\t\t\t {byte.MinValue} \t\t {byte.MaxValue}");
+Console.WriteLine($"short: \t {sizeof(short)}\t\t\t\t{short.MinValue} \t\t {short.MaxValue}");
+Console.WriteLine($"ushort:\t {sizeof(ushort)}\t\t\t\t {ushort.MinValue} \t\t {ushort.MaxValue}");
+Console.WriteLine($"int:   \t {sizeof(int)}\t\t\t\t {int.MinValue} \t {int.MaxValue}");
+Console.WriteLine($"uint: \t {sizeof(uint)}\t\t\t\t {uint.MinValue} \t\t {uint.MaxValue}");
+Console.WriteLine($"long: \t {sizeof(long)}\t\t\t\t{long.MinValue} \t\t {long.MaxValue}");
+Console.WriteLine($"ulong: \t {sizeof(ulong)}\t\t\t\t {ulong.MinValue} \t\t {ulong.MaxValue}");
+Console.WriteLine($"float: \t {sizeof(float)}\t\t\t\t{float.MinValue} \t\t {float.MaxValue}");
+Console.WriteLine($"double:  {sizeof(double)}\t\t\t\t{double.MinValue} \t\t {double.MaxValue}");
+Console.WriteLine($"decimal: {sizeof(decimal)}\t\t\t\t{decimal.MinValue} \t\t {decimal.MaxValue}");
+Console.WriteLine("-------------------------------------------------------------");
 
-Console.WriteLine("Please enter your second number: ");
-string input2Str = Console.ReadLine();
 
-int input1 = int.Parse(input1Str);
-int input2 = int.Parse(input2Str);
-int sum = (input1 + input2);
-
-Console.WriteLine($"The first number was: {input1}");
-Console.WriteLine($"The second number was: {input2}");
-Console.WriteLine($"The sum is: {sum}");
